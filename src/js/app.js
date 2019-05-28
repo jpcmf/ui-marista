2362a41db8c602ee5ee06623a1332f696e019141
João Paulo <musicjoaopaulo@gmail.com>
2019-05-28 02:19:21 -0300

    Add styles of header

--- src/js/app.js
+++ src/js/app.js
@@ -20,3 +20,36 @@ import '../sass/main.scss';
 ;(function(window, document){
   $('h1').click(() => alert('It\'s only a test'));
 })(window, document);
+
+$(document).ready(function() {
+
+  // // breakpoint and up
+  // $(window).resize(function () {
+  //   if ($(window).width() >= 980) {
+
+  //     // when you hover a toggle show its dropdown menu
+  //     $(".navbar-secondary .dropdown-toggle").hover(function () {
+  //       $(this).parent().toggleClass("show");
+  //       $(this).parent().find(".dropdown-menumega").toggleClass("show");
+  //     });
+
+  //     // hide the menu when the mouse leaves the dropdown
+  //     $(".navbar-secondary .dropdown-menumega").mouseleave(function () {
+  //       $(this).removeClass("show");
+  //     });
+
+  //     // do something here
+  //   }
+  // });
+
+  // open subnav of megamenu items on hover
+  // --------------------------------------
+  $(".nav-item--collapse").hover(
+    function () {
+      $(this).children('.collapse').collapse('show');
+    }, function () {
+      $(this).children('.collapse').collapse('hide');
+    }
+  );
+
+});
