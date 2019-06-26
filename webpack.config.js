e06e6a229609564707b88e0da52ee42349a3d258
João Paulo <musicjoaopaulo@gmail.com>
2019-06-25 23:53:31 -0300

    Add pics page structure and styles

--- webpack.config.js
+++ webpack.config.js
@@ -74,6 +74,17 @@ plugins.push(new htmlWebpackPlugin({
   template: __dirname + '/noticias.html'
 }));
 
+plugins.push(new htmlWebpackPlugin({
+  hash: true,
+  minify: {
+    html5: true,
+    collapseWhitespace: true,
+    removeComments: true
+  },
+  filename: 'fotos.html',
+  template: __dirname + '/fotos.html'
+}));
+
 plugins.push(new extractTextPlugin('style.css'));
 
 plugins.push(new webpack.ProvidePlugin({
