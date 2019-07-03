d96cd9fb6a7d4923034bf40bbe47427e68955611
João Paulo <musicjoaopaulo@gmail.com>
2019-07-02 23:25:12 -0300

    Add fale conosco page structure and styles

--- webpack.config.js
+++ webpack.config.js
@@ -107,6 +107,17 @@ plugins.push(new htmlWebpackPlugin({
   template: __dirname + '/biblioteca.html'
 }));
 
+plugins.push(new htmlWebpackPlugin({
+  hash: true,
+  minify: {
+    html5: true,
+    collapseWhitespace: true,
+    removeComments: true
+  },
+  filename: 'fale-conosco.html',
+  template: __dirname + '/fale-conosco.html'
+}));
+
 plugins.push(new extractTextPlugin('style.css'));
 
 plugins.push(new webpack.ProvidePlugin({
