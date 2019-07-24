f27b4d242aba3b523345e00b1df445a0fea07063
João Paulo <musicjoaopaulo@gmail.com>
2019-07-23 23:47:02 -0300

    Add content and styles of instituto fundador

--- webpack.config.js
+++ webpack.config.js
@@ -147,6 +147,17 @@ plugins.push(new htmlWebpackPlugin({
     collapseWhitespace: true,
     removeComments: true
   },
+  filename: 'instituto-fundador.html',
+  template: __dirname + '/instituto-fundador.html'
+}));
+
+plugins.push(new htmlWebpackPlugin({
+  hash: true,
+  minify: {
+    html5: true,
+    collapseWhitespace: true,
+    removeComments: true
+  },
   filename: 'ser-marista.html',
   template: __dirname + '/ser-marista.html'
 }));
