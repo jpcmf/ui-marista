fcb2021a500700f5cb86beabe5dc22d057fd2715
João Paulo <musicjoaopaulo@gmail.com>
2019-08-02 19:49:31 -0300

    Add content and styles of instituto capitulos nairobi

--- webpack.config.js
+++ webpack.config.js
@@ -248,6 +248,17 @@ plugins.push(new htmlWebpackPlugin({
     collapseWhitespace: true,
     removeComments: true
   },
+  filename: 'instituto-capitulos-nairobi.html',
+  template: __dirname + '/instituto-capitulos-nairobi.html'
+}));
+
+plugins.push(new htmlWebpackPlugin({
+  hash: true,
+  minify: {
+    html5: true,
+    collapseWhitespace: true,
+    removeComments: true
+  },
   filename: 'ser-marista.html',
   template: __dirname + '/ser-marista.html'
 }));
